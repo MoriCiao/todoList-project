9319b7dd5f85b391161c1fefbfa40ec139a0a201
MoriCiao <bboynoice@gmail.com>
2025-05-02 20:35:25 +0800

    add Timer tool

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import TodoList from "./TodoList";
-
+import Timer from "./TimerTool";
 const Header = () => {
   return (
-    <>
+    <div className="header-area">
       <div className="bg-img"></div>
+      <Timer />
       <TodoList />
-    </>
+    </div>
   );
 };
 
---
+++ src/TimerTool.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const Timer = () => {
+  const [name, setName] = useState("訪客");
+  const [count, setCount] = useState(0);
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    timerRef.current = setInterval(() => {
+      setCount((x) => x + 1);
+      console.log(timerRef.current);
+    }, 1000);
+
+    return () => {
+      clearInterval(timerRef.current);
+    };
+  }, []);
+
+  // Stop
+  const StopTimer = () => {
+    clearInterval(timerRef.current);
+  };
+  return (
+    <div className="timer-area">
+      <h1>Hi!{name}</h1>
+      <p>已在目前頁面停留</p>
+      <p>秒數：{count}</p>
+      <button type="button" onClick={StopTimer}>
+        Stop!
+      </button>
+    </div>
+  );
+};
+
+export default Timer;
